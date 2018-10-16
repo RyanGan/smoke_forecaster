6857759dd060115a307916c47459a8162d21ce48
RyanGan <ryan.william.gan@gmail.com>
2018-10-16 15:45:50 -0400

    Fixed how script determines if it's running local or on salix.

--- support_r_scripts/daily_bluesky_download.R
+++ support_r_scripts/daily_bluesky_download.R
@@ -1,12 +1,14 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-if(length(args)>0){
-  machine_name <- args[1]
+# RG Mod 2018-10-16: This seems to not be able to tell this is the salix server
+# so it uses old nc files. Modifying code to look at relative path name and if it has
+# /srv/ in it, then assume it's salix and not a local machine
+if(grepl('/srv/', getwd()) == TRUE){
+  machine_name <- 'salix'
 }else{
   machine_name <- "local"
 }
 print(paste("Passed arguments:"))
-print(args)
+print(paste("Running on", machine_name))
 
 # ------------------------------------------------------------------------------
 # Title: Daily BlueSky forecast download and data management
@@ -50,9 +52,6 @@ if(machine_name == "salix"){
   # Local development taking place. 
   home_path <- paste0(getwd(), "/")
 }
-
-# check directory
-print(home_path)
 
 # download bluesky daily output -----------------------------------------------
 
